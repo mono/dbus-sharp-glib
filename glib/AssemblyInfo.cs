c77e3f60ee23bc2aab69e23abb4a7476f54f200f
Alp Toker <alp@atoker.com>
2006-09-21 05:33:43 +0100

    Add AssemblyInfo

---
+++ glib/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+//[assembly: AssemblyVersion("0.0.0.*")]
+[assembly: AssemblyTitle ("NDesk.DBus.GLib")]
+[assembly: AssemblyDescription ("GLib integration for NDesk.DBus")]
+[assembly: AssemblyCopyright ("Copyright (C) Alp Toker")]
+[assembly: AssemblyCompany ("NDesk")]
