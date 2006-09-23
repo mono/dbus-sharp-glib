48573b118677b7639a1046548126131550358a66
Alp Toker <alp@atoker.com>
2006-09-23 07:38:48 +0100

    Add sample that forwards from system bus to session bus

---
+++ glib/TestSystemBus.cs
@@ -0,0 +1,69 @@
+// Copyright 2006 Alp Toker <alp@atoker.com>
+// This software is made available under the MIT License
+// See COPYING for details
+
+using System;
+using GLib;
+using Gtk;
+using NDesk.DBus;
+using org.freedesktop.DBus;
+
+public class TestGLib
+{
+	static Bus bus;
+	static Bus sysBus;
+
+	static DemoObject demo;
+
+	public static void Main ()
+	{
+		Application.Init ();
+
+		Window win = new Window ("DBus#");
+		win.SetDefaultSize (640, 480);
+		win.Destroyed += delegate {Application.Quit ();};
+		win.ShowAll ();
+
+		bus = DApplication.SessionBus;
+		sysBus = DApplication.SystemBus;
+
+		string myNameReq = "org.ndesk.gtest";
+		ObjectPath myPath = new ObjectPath ("/org/ndesk/test");
+
+		if (bus.NameHasOwner (myNameReq)) {
+			demo = DApplication.SessionConnection.GetObject<DemoObject> (myNameReq, myPath);
+		} else {
+			NameReply nameReply = bus.RequestName (myNameReq, NameFlag.None);
+
+			Console.WriteLine ("nameReply: " + nameReply);
+
+			demo = new DemoObject ();
+			sysBus.NameOwnerChanged += demo.FireChange;
+			DApplication.SessionConnection.Marshal (demo, myNameReq, myPath);
+		}
+
+		Application.Run ();
+	}
+}
+
+[Interface ("org.ndesk.gtest")]
+public class DemoObject : MarshalByRefObject
+{
+	/*
+	public DemoObject ()
+	{
+		NameOwnerChangedOnSystemBus += delegate {};
+		DApplication.SystemBus.NameOwnerChanged += NameOwnerChangedOnSystemBus;
+	}
+	*/
+
+	//is it possible to do this without needing the following method?
+	public void FireChange (string name, string old_owner, string new_owner)
+	{
+		Console.WriteLine ("Asked to fire off signal NameOwnerChangedOnSystemBus");
+		if (NameOwnerChangedOnSystemBus != null)
+			NameOwnerChangedOnSystemBus (name, old_owner, new_owner);
+	}
+
+	public event NameOwnerChangedHandler NameOwnerChangedOnSystemBus;
+}
