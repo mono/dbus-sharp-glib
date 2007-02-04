069ffde59d7a9e4d060a0c97075a4e0481553992
Alp Toker <alp@atoker.com>
2007-02-04 20:32:16 +0000

    Update AssemblyVersion

--- glib/AssemblyInfo.cs
+++ glib/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-//[assembly: AssemblyVersion("0.0.0.*")]
+[assembly: AssemblyVersion("0.3.0")]
 [assembly: AssemblyTitle ("NDesk.DBus.GLib")]
 [assembly: AssemblyDescription ("GLib integration for NDesk.DBus, the D-Bus IPC library")]
 [assembly: AssemblyCopyright ("Copyright (C) Alp Toker")]
